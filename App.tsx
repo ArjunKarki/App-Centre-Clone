d946248ad779799829c46184c2d71ea49e9313bb
Aung Kyaw Soe <33275086+ArjunKarki@users.noreply.github.com>
2023-08-09 11:57:53 +0630

    commit from dev

--- App.tsx
+++ App.tsx
@@ -62,6 +62,8 @@ function App(): JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  console.log('changes one in dev');
+
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar
