b18b5f898d2459fc9c2c24d75a3a5bec2cf6cb6b
Aung Kyaw Soe <33275086+ArjunKarki@users.noreply.github.com>
2023-08-09 12:01:38 +0630

    from feature 1

--- App.tsx
+++ App.tsx
@@ -72,7 +72,7 @@ function App(): JSX.Element {
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
         <Header />
-        <View style={{backgroundColor: 'green'}}>
+        <View style={{backgroundColor: 'yellow'}}>
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
